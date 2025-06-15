5aecb8829cc62094b727307bf33d3df1b233e76a
Serg <gorfeed66@gmail.com>
2025-06-15 20:53:26 +0500

    Test CI/CD deployment

--- deploy.bat
+++ deploy.bat
@@ -7,19 +7,19 @@ echo Pulling latest changes...
 git pull origin main
 
 echo Stopping existing containers...
-sudo docker-compose down
+docker-compose down
 
 echo Cleaning up...
-sudo docker system prune -f
+docker system prune -f
 
 echo Building and starting services...
-sudo docker-compose up -d --build
+docker-compose up -d --build
 
 echo Waiting for services...
 timeout /t 30 /nobreak
 
 echo Checking status...
-sudo docker ps
+docker ps
 
 echo Testing APIs...
 curl http://localhost:5001/patients
